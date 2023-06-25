98973600925f58754fb7c5a30420e154f4bf8a0d
shubhamtikka <t.shubham1910@gmail.com>
2023-06-25 17:49:56 +0530

    Corrected routing of base pages

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { Contact } from '../model/Contact';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
+  contacts: Contact[] = [
+    new Contact('John Doe', '1234567890', 'john@example.com', new Date()),
+    new Contact('Jane Smith', '9876543210', 'jane@example.com', new Date()),
+    // Add more dummy contacts as needed
+  ];
 
-  ngOnInit(): void {
+  editContact(contact: Contact) {
+    // Implement the edit functionality as needed
+    console.log('Edit contact:', contact);
   }
 
+  deleteContact(contact: Contact) {
+    // Implement the delete functionality as needed
+    console.log('Delete contact:', contact);
+    this.contacts = this.contacts.filter((c) => c !== contact);
+  }
 }
---
+++ src/app/model/Contact.ts
@@ -0,0 +1,8 @@
+export class Contact {
+  constructor(
+    public name: string,
+    public contact: string,
+    public emailId: string,
+    public birthDay: Date
+  ) {}
+}
